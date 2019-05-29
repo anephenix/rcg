4028da12d6d5a0429de327d50fec839dc4da0ec7
Paul Jensen <paulbjensen@gmail.com>
2019-05-29 22:08:13 +0200

    Added a test for the binary when running with default options

--- __tests__/bin/rcg.test.js
+++ __tests__/bin/rcg.test.js
@@ -4,14 +4,57 @@
 const assert = require('assert');
 const childProcess = require('child_process');
 const util = require('util');
+const path = require('path');
+const fs = require('fs');
+
+// Promisified functions
 const exec = util.promisify(childProcess.exec);
+const exists = util.promisify(fs.exists);
+const readdir = util.promisify(fs.readdir);
+
+// File Dependencies
 const { version } = require('../../package.json');
 
 describe('rcg binary', () => {
 	describe('default', () => {
-		it.todo(
-			'should create a component with a given name in the src/components directory'
+		const title = 'MyTestComponent';
+		const folderName = 'my-test-component';
+		const folderPath = path.join(
+			process.cwd(),
+			'src',
+			'components',
+			folderName
 		);
+
+		const seed = async () => {
+			return await exec(
+				`mkdir -p ${path.join(process.cwd(), 'src', 'components')}`
+			);
+		};
+
+		const cleanup = async () => {
+			return await exec(`rm -rf ${path.join(process.cwd(), 'src')}`);
+		};
+		beforeEach(async () => await seed());
+		afterEach(async () => await cleanup());
+
+		it('should create a component with a given name in the src/components directory', async () => {
+			const command = `./bin/rcg ${title}`;
+			const { stdout, stderr } = await exec(command);
+			assert.equal(stdout, '');
+			assert.equal(stderr, '');
+			const folderExists = await exists(folderPath);
+			assert(folderExists);
+			const files = await readdir(folderPath);
+			const expectedFiles = [
+				`${title}.js`,
+				`${title}.test.js`,
+				`${title}.scss`
+			];
+			for (const file of expectedFiles) {
+				assert(files.indexOf(file) !== -1);
+			}
+		});
 	});
 	describe('--version', () => {
 		it('should return the version of the program', async () => {
